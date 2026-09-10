680b2b07f644980f00c216e4d17674f344641d09
PHAHS <phahs.genoffice@mun.ca>
2026-09-10 10:57:48 -0230

    Adding Cross Appointed Faculty

--- contacts.js
+++ contacts.js
@@ -57,6 +57,11 @@ const contactListData = [
   { firstName: "Rose", lastName: "Ricciardelli", area: "Cross-Appointed", positionType: "Professor, Marine Institute", ext: "778-0361 ", email: "rose.ricciardelli@mi.mun.ca", office: "", location: "Marine Institute", profileUrl: "https://www.mi.mun.ca/departments/oceansafetyresearchunitosru/ourteam/" },
   { firstName: "Sevtap", lastName: "Savas", area: "Cross-Appointed", positionType: "Professor, Division of BioMedical Sciences", ext: "864-6507 ", email: "savas@mun.ca", office: "Division of Biomedical Sciences", location: "", profileUrl: "https://www.mun.ca/medicine/faculty-and-staff-resources/faculty-a-z/savas-sevtap.php" },
   { firstName: "Eric", lastName: "Tenkorang", area: "Cross-Appointed", positionType: "Professor, Sociology", ext: "864-6507 ", email: "", office: "", location: "", profileUrl: "" },
+  { firstName: "Kim", lastName: "Cullen", area: "Cross-Appointed", positionType: "Assistant Professor, Human Kinetics", ext: "", email: "kcullen@mun.ca", office: "", location: "", profileUrl: "" },
+  { firstName: "Eric", lastName: "Tenkorang", area: "Cross-Appointed", positionType: "Professor, Sociology", ext: "864-6507 ", email: "", office: "", location: "", profileUrl: "" },
+  { firstName: "Lea", lastName: "Lewis", area: "Cross-Appointed", positionType: "", ext: "", email: "leah.lewis@mun.ca", office: "", location: "", profileUrl: "" },
+  { firstName: "Geoff", lastName: "Warden", area: "Cross-Appointed", positionType: "Clinical Assistant Professor, Anesthesia", ext: "", email: "gwarden@mun.ca", office: "", location: "", profileUrl: "" },
+  { firstName: "Nicholas", lastName: "Smith", area: "Cross-Appointed", positionType: "Clinical Assistant Professor, Discipline of Surgery", ext: "", email: "ncs662@mun.ca", office: "", location: "", profileUrl: "" },
   { firstName: "Zaina", lastName: "Albalawi", area: "Cross-Appointed", positionType: "Associate Professor, Medicine", ext: "", email: "zalbalawi@mun.ca", office: "", location: "", profileUrl: "" },
   { firstName: "Brenda", lastName: "Wilson", area: "Faculty", positionType: "Professor", ext: "", email: "bwilson@mun.ca", office: "", location: "", profileUrl: "https://www.mun.ca/medicine/faculty-and-staff-resources/faculty-a-z/wilson-brenda.php" },
 ];
