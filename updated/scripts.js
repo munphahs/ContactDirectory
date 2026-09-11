a4d98984a07c5798c0f112047d2d9992a74c8fc6
PHAHS <phahs.genoffice@mun.ca>
2026-09-11 11:12:10 -0230

    Update scripts.js

--- updated/scripts.js
+++ updated/scripts.js
@@ -60,14 +60,15 @@ function searchForms() {
 
 // Sample form data (replace with actual data or load dynamically from a database)
 const formListData = [
-  {formName: "Travel Request", category: "Travel", link: "https://www.mun.ca/finance/media/production/memorial/administrative/financial-and-administrative-services/media-library/forms/TRAVEL_REQUEST.pdf"},
-  {formName: "Travel Advance Request", category: "Travel", link: "https://www.mun.ca/medicine/media/production/medicine/documents/Travel-Advance-Form-August-2019.pdf"},
-  {formName: "Travel Claim", category: "Travel", link: "https://www.mun.ca/finance/media/production/memorial/administrative/financial-and-administrative-services/media-library/forms/Travel_Expense_Claim.xlsx"},
-{
+    {formName: "Faculty Onboarding Guide", category: "Other", link: "https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fmunphahs%2FContactDirectory%2Frefs%2Fheads%2Fmain%2FFaculty%2520Onboarding%2520Guide.docx&wdOrigin=BROWSELINK"},
+    {
   formName: "PHAHS Travel Information Sheet",
   category: "Travel",
   link: "https://munphahs.github.io/ContactDirectory/PHAHS%20Travel%20Information%20Sheet.docx"
 },
+    {formName: "Travel Request", category: "Travel", link: "https://www.mun.ca/finance/media/production/memorial/administrative/financial-and-administrative-services/media-library/forms/TRAVEL_REQUEST.pdf"},
+  {formName: "Travel Advance Request", category: "Travel", link: "https://www.mun.ca/medicine/media/production/medicine/documents/Travel-Advance-Form-August-2019.pdf"},
+  {formName: "Travel Claim", category: "Travel", link: "https://www.mun.ca/finance/media/production/memorial/administrative/financial-and-administrative-services/media-library/forms/Travel_Expense_Claim.xlsx"},
   {formName: "Meals Per Diem plus incidental allowances", category: "Travel", link: "https://www.mun.ca/finance/financial-services/travel/schedule-of-reimbursable-expenses/#PerDiem"},
   {formName: "Daily Car Kilometer Report", category: "Travel", link: "https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fwww.mun.ca%2Fmedicine%2Fmedia%2Fproduction%2Fmedicine%2Fdocuments%2FDaily-Car-Kilo-Report-Form-(2).docx&wdOrigin=BROWSELINK"},
   {formName: "Print & Mail Requisition", category: "Purchase", link: "https://www.mun.ca/printandmail/media/production/memorial/administrative/print-and-mail-services/media-library/pdfs/PrintAndMailReqForWeb.pdf"},
